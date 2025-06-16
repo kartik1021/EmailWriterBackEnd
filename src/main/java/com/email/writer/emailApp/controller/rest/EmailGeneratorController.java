a6d125c026c72e0b07969f6b9ce78e7ae827b939
kartik1021 <jkartik782@gmail.com>
2025-06-16 21:41:16 +0530

    added logs

--- src/main/java/com/email/writer/emailApp/controller/rest/EmailGeneratorController.java
+++ src/main/java/com/email/writer/emailApp/controller/rest/EmailGeneratorController.java
@@ -5,18 +5,21 @@ import com.email.writer.emailApp.business.EmailGeneratorService;
 import com.email.writer.emailApp.domain.EmailRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
 @CrossOrigin(origins = "*")
+@Slf4j
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
 
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        log.info( "request bode --------->>>>" + emailRequest.toString());
         String response = emailGeneratorService.generateEmailReply(emailRequest);
         return ResponseEntity.ok(response);
     }
