a787bdcbe143661715288560a4a4fd74a48b4231
kartik1021 <jkartik782@gmail.com>
2025-06-16 21:56:10 +0530

    added more logs

--- src/main/java/com/email/writer/emailApp/controller/rest/EmailGeneratorController.java
+++ src/main/java/com/email/writer/emailApp/controller/rest/EmailGeneratorController.java
@@ -19,8 +19,10 @@ public class EmailGeneratorController {
 
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
-        log.info( "request bode --------->>>>" + emailRequest.toString());
-        String response = emailGeneratorService.generateEmailReply(emailRequest);
-        return ResponseEntity.ok(response);
+        log.info("emailContent: {}", emailRequest.getEmailContent());
+    log.info("tone: {}", emailRequest.getTone());
+
+    String mockReply = "Hi there! This is a test reply with tone: " + emailRequest.getTone();
+    return ResponseEntity.ok(mockReply);
     }
 }
